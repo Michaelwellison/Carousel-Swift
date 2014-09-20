06e5f1265da886d2c2f6349f74d76b5603b812b5
Michael W. Ellison <michaelwellison@gmail.com>
2014-09-20 13:22:50 -0700

    added welcome screen

--- Carousel-Swift/SignInViewController.swift
+++ Carousel-Swift/SignInViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class SignInViewController: UIViewController, UITextFieldDelegate {
     
+    // MARK: Outlets
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginFormView: UIView!
     @IBOutlet weak var loginTextImage: UIImageView!
+    
+    // MARK: View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,28 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Configuration
+    
+    func userEmailAndPasswordCheck() {
+        
+        var emailFieldInput = String(emailTextField.text)
+        var passwordFieldInput = String(passwordTextField.text)
+        
+        if emailFieldInput == "michaelwellison@gmail.com" && passwordFieldInput == "password" {
+            
+            self.performSegueWithIdentifier("signIn", sender: self)
+            println("Success!")
+            
+        } else {
+            
+            var errorView = UIAlertController(title: "Incorrect Password", message: "The password or username you entered is incorrect. Please try again.", preferredStyle: UIAlertControllerStyle.Alert)
+            errorView.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(errorView, animated: true, completion: nil)
+            
+            println("Error")
+        }
     }
     
     // MARK: Text Field Delegate
@@ -50,11 +76,14 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+        // MARK: Actions
     
     @IBAction func onTapOutsideTextView(sender: AnyObject) {
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
     }
 
+    // MARK: Navigation
+    
 
 }
